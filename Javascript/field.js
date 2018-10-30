4db747f09d64bc070b05ac1b6cc13dd387c5c941
Arbeitslaptop <F.Willrich@ostfalia.de>
2018-10-30 12:57:43 +0100

    Hinzufügen des Spielfeldes und des Designs

--- Javascript/field.js
+++ Javascript/field.js
@@ -1,5 +1,41 @@
 /*jslint browser: true */
-/*eslint-env browser*/
-var canvas = document.querySelector('canvas');
-canvas.width = 640;
-canvas.height = 480;
+/* eslint-env browser */
+/* global window */
+"use strict";
+
+var context = null;
+var canvas = null;
+
+
+
+
+function prepareCanvas() {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext("2d");
+
+    canvas.width = 640;
+    canvas.height = 480;
+
+    context.moveTo(canvas.width / 2, 0);
+    context.lineTo(canvas.width / 2, canvas.height);
+    context.strokeStyle = "#FF0000";
+    context.stroke();
+
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2, 10, 0, 2 * Math.PI)
+    context.fillStyle = '#FF0000';
+    context.fill();
+
+    context.beginPath();
+    context.arc(0, canvas.height / 2, 80, 1.5 * Math.PI, 0.5 * Math.PI)
+    context.strokeStyle = "#0000FF";
+    context.stroke();
+
+    context.beginPath();
+    context.arc(canvas.width, canvas.height / 2, 80, 0.5 * Math.PI, 1.5 * Math.PI)
+    context.stroke();
+
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2, 80, 0, 2 * Math.PI)
+    context.stroke();
+}
