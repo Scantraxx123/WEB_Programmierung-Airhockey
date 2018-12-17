bc7b54d0782926a5b9935baa87e26fdf0052b6c5
Arbeitslaptop <F.Willrich@ostfalia.de>
2018-12-17 11:57:38 +0100

    Null Fehler abgefangen bei leererm Highscore

--- Javascript/highscore.js
+++ Javascript/highscore.js
@@ -4,7 +4,7 @@
 "use strict";
 
 
-var highScoreEntries = [];
+var highScoreEntries;
 
 function highScoreEntry(name, time) {
     this.name = name;
@@ -19,22 +19,28 @@ function setLocalStorage() {
 function addEntry(name, time) {
     getHighscore();
     var newUser = new highScoreEntry(name, time);
-    var i;
-    for (i = 0; i < highScoreEntries.length; i++) {
-        if (highScoreEntries[i].time > time) {
-            highScoreEntries.splice(i, 0, newUser);
-            if (highScoreEntries.length >= 10) {
-                highScoreEntries.splice(-1, 1);
+    if (highScoreEntries != null) {
+        var i;
+        for (i = 0; i < highScoreEntries.length; i++) {
+            if (highScoreEntries[i].time > time) {
+                highScoreEntries.splice(i, 0, newUser);
+                if (highScoreEntries.length >= 10) {
+                    highScoreEntries.splice(-1, 1);
+                }
+                setLocalStorage();
+                return;
+
             }
-            break;
         }
     }
-    if (highScoreEntries.length < 10) {
+
+    if (highScoreEntries == null) {
+        highScoreEntries = [];
+        highScoreEntries.push(newUser);
+    } else if (highScoreEntries.length < 10) {
         highScoreEntries.push(newUser);
     }
     setLocalStorage();
-
-
 
 }
 
@@ -42,25 +48,29 @@ function getHighscore() {
     var retrievedObject = localStorage.getItem('highScoreEntries');
     highScoreEntries = JSON.parse(retrievedObject);
 
-
-    highScoreEntries.sort(function (a, b) {
-        return a.time - b.time
-    });
+    if (highScoreEntries != null) {
+        highScoreEntries.sort(function (a, b) {
+            return a.time - b.time
+        });
+    }
 
 }
 
 
 function displayScore() {
     getHighscore();
-    var i;
-    var highscore = "";
-    for (i = 0; i < highScoreEntries.length; i++) {
-        highscore += i + 1 + " " + highScoreEntries[i].name + " " + highScoreEntries[i].time;
-        if (highScoreEntries.length - 1 != i) {
-            highscore += "<br>";
+    if (highScoreEntries != null) {
+        var i;
+        var highscore = "";
+        for (i = 0; i < highScoreEntries.length; i++) {
+            highscore += i + 1 + " " + highScoreEntries[i].name + " " + highScoreEntries[i].time;
+            if (highScoreEntries.length - 1 != i) {
+                highscore += "<br>";
+            }
+
         }
 
+        document.getElementById("demo").innerHTML = highscore;
     }
-    document.getElementById("demo").innerHTML = highscore;
 
 }
